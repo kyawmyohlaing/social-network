0f6c3cdf5be19e1fe9ac91cff2e9ad9bbbb75475
kyawmyohlaing <kyawthanthat@gmail.com>
2020-04-03 11:36:27 +0630

    Updates

---
+++ precache-manifest.527f2d5d8f2d0479a10bf246f9459cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d03915b996e4b12c921c",
+    "url": "/social-network/static/css/main.f6713068.chunk.css"
+  },
+  {
+    "revision": "d03915b996e4b12c921c",
+    "url": "/social-network/static/js/main.d03915b9.chunk.js"
+  },
+  {
+    "revision": "d0b2e3c3f0659a77f428",
+    "url": "/social-network/static/css/1.4ba328f4.chunk.css"
+  },
+  {
+    "revision": "d0b2e3c3f0659a77f428",
+    "url": "/social-network/static/js/1.d0b2e3c3.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "5a9f32e1a1c21f907867638b9e5cb8be",
+    "url": "/social-network/index.html"
+  }
+];
